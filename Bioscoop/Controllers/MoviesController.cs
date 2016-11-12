71fed571ca01bdae03d9dc9afca2298c09d98200
Reinoud van Zoelen <354261@student.fontys.nl>
2016-11-12 14:24:21 +0100

    First version

---
+++ Bioscoop/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bioscoop.Models;
+
+namespace Bioscoop.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>();
+
+            movies.Add(new Movie(0, "Shrek!", 100));
+            movies.Add(new Movie(1, "Pokémon", 80));
+            movies.Add(new Movie(2, "Three Musketeers", 90));
+            movies.Add(new Movie(3, "Mickey Mouse", 110));
+            movies.Add(new Movie(4, "100 Arabian Nights", 140));
+
+            return View(movies);
+        }
+
+        public ActionResult newMovie()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bioscoop/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bioscoop.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Length { get; set; }
+
+
+		public Movie(int id, string name, int length)
+		{
+			Id = id;
+			Name = name;
+			Length = length;
+		}
+
+	}
+}
